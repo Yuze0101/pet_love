0258b48812a734c2a69ad3e8c8703d50fb5440fb
Nevermore <mengmiao.0101@icloud.com>
2023-02-20 21:59:19 +0800

    更新接口封装

--- api/index.ts
+++ api/index.ts
@@ -4,3 +4,10 @@ export const login = (params: {}) => post({ url: '/pet/account/login.json', para
 export const register = (params: {}) => post({ url: '/pet/account/signUp.json', params });
 export const resetPassword = (params: {}) => post({ url: '/pet/account/resetPassword.json', params });
 export const getVerificationCode = (params: {}) => get({ url: '/pet/account/getVerificationCode.json', params });
+
+export const queryDetail = (params: {}) => get({ url: '/pet/userInfo/queryDetail.json', params });
+export const updateUserInfo = (params: {}) => post({ url: '/pet/userInfo/updateUserInfo.json', params });
+export const upload = (params: {}) => post({ url: ' /pet/tool/upload.json', params });
+export const createPet = (params: {}) => post({ url: '/pet/userInfo/createPet.json', params });
+export const follow_count = (params: {}) => post({ url: '/pet/userInfo/editPet.json', params });
+export const deletePet = (params: {}) => get({ url: ' /pet/userInfo/deletePet.json', params });
