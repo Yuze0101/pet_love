4dc021274f5c22d137ad623c03e264db8da3033e
SummiluxM <turbocharger1024@gmail.com>
2023-02-20 15:00:52 +0800

    添加UserCenterScreen

---
+++ screens/UserCenterScreen.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+import { Text, Layout } from '@ui-kitten/components';
+
+import { RootTabScreenProps } from '../types';
+
+export default function UserCenterScreen({ navigation }: RootTabScreenProps<'TabFour'>) {
+  return (
+    <Layout style={styles.container}>
+      <Text style={styles.title}>UserCenterScreen</Text>
+      <Layout style={styles.separator} />
+    </Layout>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'red',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
