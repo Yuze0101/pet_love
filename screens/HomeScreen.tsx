589a0f51181873f572aae259e18fb8a9dbe260fc
Nevermore <mengmiao.0101@icloud.com>
2023-03-06 11:30:08 +0800

    主页样式调整

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,20 @@
 import { useState, useCallback } from 'react';
-import { RefreshControl } from 'react-native';
-import { Layout, Text, ViewPager, TabBar, Tab, Divider, List, ListItem, Card, Avatar } from '@ui-kitten/components';
+import { RefreshControl, Image } from 'react-native';
+import {
+  Layout,
+  Text,
+  ViewPager,
+  TabBar,
+  Tab,
+  Divider,
+  List,
+  Card,
+  Avatar,
+  Button,
+  ButtonGroup,
+  Icon,
+  useTheme,
+} from '@ui-kitten/components';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 
@@ -21,27 +35,74 @@ type CreativeCardProps = {
   releaseTime: Date;
   contentText: string;
 };
+
+type ListItem = {
+  id: number;
+  petId: number;
+  createDate: Date;
+  modifyDate: Date;
+  picList: string[];
+  content: string;
+  likesCount: number;
+  commentCount: number;
+  hotValue: number;
+  tipOffCount: number;
+  status: 'PUBLIC' | 'PRIVATE';
+  type: 'CAT' | 'DOG' | 'OTHER';
+  extInfo: string;
+};
+
+type ListData = ListItem[];
+
 const RenderTitle = (props: RenderTitleProps) => (
-  <Text status={props.isActive ? 'primary' : 'basic'} category="h3">
+  <Text status={props.isActive ? 'primary' : 'basic'} category="h4">
     {props.title}
   </Text>
 );
 
-const CardFooter = () => (
-  <Layout>
-    <Text>footer</Text>
-  </Layout>
-);
+const CardFooter = () => {
+  const theme = useTheme();
+  return (
+    <Layout style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+      <Layout style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Avatar size="large" source={require('../assets/images/icon.png')}></Avatar>
+        <Layout style={{ paddingLeft: pxToDp(5) }}>
+          <Text category="">owner name</Text>
+        </Layout>
+      </Layout>
+      <ButtonGroup appearance="ghost" style={{ margin: 2 }}>
+        <Button style={{ flexDirection: 'column' }} accessoryLeft={<Icon name="message-circle-outline" />}>
+          999+
+        </Button>
+        <Button
+          style={{ flexDirection: 'column' }}
+          accessoryLeft={<Icon fill={theme['color-danger-500']} name="heart" />}
+        >
+          999+
+        </Button>
+      </ButtonGroup>
+    </Layout>
+  );
+};
 
 const CardHeader = () => (
-  <Layout>
-    <Text>Header</Text>
+  <Layout style={{ flexDirection: 'row', alignItems: 'center' }}>
+    <Avatar size="large" source={require('../assets/images/icon.png')}></Avatar>
+    <Layout style={{ paddingLeft: pxToDp(5) }}>
+      <Text category="s1">Header</Text>
+      <Text category="c2" appearance="hint">
+        Header
+      </Text>
+    </Layout>
   </Layout>
 );
 
 const CreativeCard = (props: CreativeCardProps) => (
-  <Card style={{ width: '100%', padding: 0 }} header={CardHeader} footer={CardFooter}>
-    <Text>content</Text>
+  <Card appearance="filled" header={CardHeader} footer={CardFooter}>
+    <Image source={require('../assets/images/locked.png')}></Image>
+    <Text category="p1" numberOfLines={3}>
+      contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent
+    </Text>
   </Card>
 );
 
@@ -59,13 +120,13 @@ export default function HomeScreen({}: Props) {
   }, []);
 
   const renderItem = () => (
-    <ListItem>
+    <Layout style={{ padding: pxToDp(8) }}>
       <CreativeCard />
-    </ListItem>
+    </Layout>
   );
   return (
-    <Layout>
-      <Layout style={{ paddingTop: insets.top }}>
+    <Layout style={{ flex: 1 }}>
+      <Layout style={{ flex: 1, paddingTop: insets.top, paddingBottom: insets.bottom }}>
         <TabBar
           style={{ paddingLeft: pxToDp(110), paddingRight: pxToDp(110) }}
           selectedIndex={selectedIndex}
@@ -80,16 +141,17 @@ export default function HomeScreen({}: Props) {
           <Layout level="2" style={{ height: '100%' }}>
             <List
               refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
-              data={[1, 2]}
+              data={[1, 2, 3]}
               renderItem={renderItem}
+              // style={{ paddingLeft: pxToDp(8), paddingRight: pxToDp(8) }}
             ></List>
           </Layout>
           <Layout level="2" style={{ height: '100%' }}>
-            <List
+            {/* <List
               refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
               data={[1, 2]}
-              renderItem={renderItem}
-            ></List>
+              renderItem={() => renderItem()}
+            ></List> */}
           </Layout>
         </ViewPager>
       </Layout>
